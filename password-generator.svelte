b4b5429577506dc98dcb0cdd21562f3d0faa82c7
Ravengal <132362783+ravengalqueen@users.noreply.github.com>
2025-01-19 02:48:57 +0100

    Add files via upload
    changes to password generation

--- password-generator.svelte
+++ password-generator.svelte
@@ -1,5 +1,5 @@
 <script>
-//ignore this is just a wordlist, jump to line 1960 for the code
+    //ignore this is just a wordlist, jump to line 1960 for the code
     const wordList = [
         "ability",
         "able",
@@ -1954,58 +1954,67 @@
         "zoo",
         "zulu",
     ];
-    let specialChars = ["?","\\", "/", "!", "\"", "$", ".", ",", "#", "*", "+", "-"]
+    let specialChars = ["?", "\\", "/", "!", "\"", "$", ".", ",", "#", "*", "+", "-"]
 
-    function getRanChar(){
-        let char = parseInt(Math.random()* specialChars.length)
-        return specialChars[char]
+    import {writable} from 'svelte/store';
+
+    import eye from '$lib/images/eye.png';
+
+    // Reactive store for the password
+    let password = writable('Password Here!');
+    let passHidden = writable('**************');
+
+    function getRanChar() {
+        let char = Math.floor(Math.random() * specialChars.length);
+        return specialChars[char];
     }
-    function getRanNumber(){
-        return parseInt(Math.random() * 10)
+
+    function getRanNumber() {
+        return Math.floor(Math.random() * 10);
     }
-    let password = $state('Password here!');
+
     function capitalizeFirstLetter(val) {
         return String(val).charAt(0).toUpperCase() + String(val).slice(1);
     }
-    function getRandomWord(){
-        return parseInt(Math.random()*(1952-1) + 1)
+
+    function getRandomWord() {
+        return Math.floor(Math.random() * wordList.length);
     }
+
     function changetext() {
-        let word1 = capitalizeFirstLetter(wordList[getRandomWord()] )
-        let word2 = capitalizeFirstLetter(wordList[getRandomWord()] )
-        let word3 = capitalizeFirstLetter(wordList[getRandomWord()] )
+        let word1 = capitalizeFirstLetter(wordList[getRandomWord()]);
+        let word2 = capitalizeFirstLetter(wordList[getRandomWord()]);
+        let word3 = capitalizeFirstLetter(wordList[getRandomWord()]);
         let number1 = getRanNumber();
         let number2 = getRanNumber();
-        let specialChar = getRanChar()
-        let specialChar2 = getRanChar()
+        let specialChar = getRanChar();
+        let specialChar2 = getRanChar();
         let dice = Math.random() < 0.5;
-        if (dice === true){password = word1 + word2 + word3 + number1 + number2 + specialChar}
-        else {password = specialChar2 + word1 + word2 + word3 + number1 + number2 + specialChar}
 
+        let newPassword =
+            dice
+                ? word1 + word2 + word3 + number1 + number2 + specialChar
+                : specialChar2 + word1 + word2 + word3 + number1 + number2 + specialChar;
+
+        password.set(newPassword);
+        passHidden.set('*'.repeat(newPassword.length));
     }
 
+    function copyToClipboard() {
+        password.subscribe((val) => navigator.clipboard.writeText(val));
+    }
 
-
+    let showPassword = true;
 </script>
 
 <h1>Password Generator</h1>
-<div class="checkbox-wrapper-49">
-    <!--  <div class="block">
-        <input data-index="0" id="cheap-49" type="checkbox" />
-        <label for="cheap-49"></label>
-        <br>
-        <p>letters to numbers</p>
-      </div>
-      <div class="block">
-          <input data-index="0" id="cheap-50" type="checkbox" />
-          <label for="cheap-50"></label>
-          <br>
-          <p>Random Uppercase</p>
-        </div>-->
-</div>
-<button id="gen" onclick={()=>{changetext()}}>Start!</button>
-<textarea typeof="password" readonly="readonly" id="text">{password}</textarea>
+<button id="gen" on:click={changetext}>Generate!</button>
+<div id="text">{showPassword ? $password : $passHidden}</div>
 <div class="res">
-    <button id="copy" onclick={() => navigator.clipboard.writeText(password)}><img src="https://cdn-icons-png.flaticon.com/512/54/54702.png" height="20"></button>
-    <button id="see"><img src="https://cdn.pixabay.com/photo/2016/12/18/11/04/eye-1915454_960_720.png" height="20"></button>
+    <button id="copy" class="button-symbol" on:click={copyToClipboard}>
+        <img class="img-small" src="https://cdn-icons-png.flaticon.com/512/54/54702.png"alt="Copy">
+    </button>
+   <!-- <button id="see" class="button-symbol" on:click={() => (showPassword = !showPassword)}>
+        <img class="img-small" src="{eye}"  alt="Toggle">
+    </button>-->
 </div>
